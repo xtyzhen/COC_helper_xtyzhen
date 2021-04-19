e52f93e751cde94a04f009308aaca63e8e186222
xtian0313 <25572441+xtian0313@users.noreply.github.com>
2021-04-19 20:53:45 +0800

    大的修复和更新
    主要内容：
    1、修复在自动补充军队的时候的逻辑问题导致的报错
    2、提升了自动捐兵的识别速度和精度

--- locate.py
+++ locate.py
@@ -11,14 +11,16 @@ import time
 import random
 import sys
 import keyboard
+import requests
+
+
 
 what={'1':'野蛮人','2':'弓箭手','3':'胖子','4':'哥布林','5':'炸弹人','6':'气球','7':'法师','8':'天使','9':'龙','10':'皮卡','11':'龙宝宝','12':'矿工','13':'电龙','14':'雪怪','15':'苍蝇','16':'野猪','17':'武神','18':'石头人','19':'女巫','20':'天狗','21':'投石手','22':'冰人','23':'英雄猎手','a':'雷电','b':'治疗','c':'狂暴','d':'弹跳','e':'冰冻','f':'复制','g':'隐身法术','h':'毒药','i':'地震','j':'疾速','k':'骷髅','l':'蝙蝠','G1':'攻城车','G2':'飞艇','G3':'大气球'}
 def mistaken():
     time.sleep(1)
     try:
-        print('程序出现异常，可能由于卡顿')
-        print('如果多次输出错误，可能是分辨率异常。请选择1440*810')
-        print('程序重新启动')
+        print('*****程序出错，跳过*****')
+        print('——————————开始运行——————————')
     except:
         mistaken()
 
@@ -48,28 +50,27 @@ def autotrain(troops):
     time.sleep(1)
     last=None
     for t in troops:
-        print('需要训练'+what[str(t)])
-        if type(t)!=type(last):
-            if type(t)==int:
-                print(str(t)+'开始训练军队'+what[str(t)]+str(troops[t])+'个')
-                Part=pyautogui.locateOnScreen('./others/train_troops.png', confidence=0.8, grayscale=False)
+        print('需要训练'+what[str(t)]+str(troops[t])+'个')
+        if type(t)==int and type(t)!=type(last):
+            print(str(t)+'开始训练军队'+what[str(t)]+str(troops[t])+'个')
+            Part=pyautogui.locateOnScreen('train_troops.png', confidence=0.8, grayscale=False)
+            Center=pyautogui.center(Part)
+            click(Center.x+random.randint(-10,10),Center.y+random.randint(-10,10))#识别并打开造兵    
+            time.sleep(0.5)
+        else:
+            if type(t)==str and t>='a' and (type(last)==type(None) or type(last)==int or (last<'a')): #type(t)==str
+                print(str(t)+'开始配置法术'+what[str(t)]+str(troops[t])+'个')
+                Part=pyautogui.locateOnScreen('spells.png', confidence=0.8, grayscale=False)
                 Center=pyautogui.center(Part)
                 click(Center.x+random.randint(-10,10),Center.y+random.randint(-10,10))#识别并打开造兵    
-                time.sleep(1)
+                time.sleep(0.5)
             else:
-                if type(t)==str and t<='a':
+                if type(t)==str and t<'a' and (type(last)==type(None) or type(last)==int or last>='a'):
+                    Part=pyautogui.locateOnScreen('machine.png', confidence=0.8, grayscale=False)
                     print(str(t)+'开始建造攻城器械'+what[str(t)]+str(troops[t])+'个')
-                    Part=pyautogui.locateOnScreen('./others/machine.png', confidence=0.8, grayscale=False)
                     Center=pyautogui.center(Part)
                     click(Center.x+random.randint(-10,10),Center.y+random.randint(-10,10))#识别并打开造兵    
-                    time.sleep(1)
-                else:
-                    # if type(t)!=type(last):
-                        Part=pyautogui.locateOnScreen('./others/spells.png', confidence=0.8, grayscale=False)
-                        print(str(t)+'开始配置法术'+what[str(t)]+str(troops[t])+'个')
-                        Center=pyautogui.center(Part)
-                        click(Center.x+random.randint(-10,10),Center.y+random.randint(-10,10))#识别并打开造兵    
-                        time.sleep(1)
+                    time.sleep(0.5)
         photo='./train_troops/'+str(t)+'.png'
         if pyautogui.locateOnScreen(photo, confidence=0.8, grayscale=True)==None and type(t)==int:
             if t>=14:
@@ -131,7 +132,7 @@ def autotrain(troops):
 def give_troops(i,troops):
     print(i)
     photo='./troops/'+str(i)+'.png'
-    Part=pyautogui.locateOnScreen(photo, confidence=0.9, grayscale=False)
+    Part=pyautogui.locateOnScreen(photo, confidence=0.8, grayscale=False)
     while Part!=None:       # <class 'pyscreeze.Box'> 
         Center=pyautogui.center(Part)
         rgb=pyautogui.screenshot().getpixel((int(Center.x+random.randint(-10,10)),int(Center.y+random.randint(-10,10))))
@@ -145,7 +146,7 @@ def give_troops(i,troops):
         else:
             troops[i]=1
         time.sleep(0.8)
-        Part=pyautogui.locateOnScreen(photo, confidence=0.9, grayscale=False)
+        Part=pyautogui.locateOnScreen(photo, confidence=0.8, grayscale=False)
     return troops
 
 def give_G(i,troops):
@@ -166,7 +167,7 @@ def give_G(i,troops):
 
 def give_spells(i,troops):
     photo='./troops/'+str(i)+'.png'
-    Part=pyautogui.locateOnScreen(photo, confidence=0.96, grayscale=False)
+    Part=pyautogui.locateOnScreen(photo, confidence=0.8, grayscale=False)
     while Part!=None:       # <class 'pyscreeze.Box'> 
         Center=pyautogui.center(Part)
         rgb=pyautogui.screenshot().getpixel((int(Center.x+random.randint(-10,10)),int(Center.y+random.randint(-10,10))))
@@ -180,7 +181,7 @@ def give_spells(i,troops):
         else:
             troops[i]=1
         time.sleep(0.7)
-        Part=pyautogui.locateOnScreen(photo, confidence=0.96, grayscale=False)
+        Part=pyautogui.locateOnScreen(photo, confidence=0.8, grayscale=False)
     return troops
 
 def main():
@@ -188,6 +189,7 @@ def main():
         Part=pyautogui.locateOnScreen('./others/reload.png', confidence=0.95, grayscale=False)
         if Part!=None:
             Center=pyautogui.center(Part)
+            print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
             print('休息15分钟')
             time.sleep(900)
             Part=pyautogui.locateOnScreen('./others/reload.png', confidence=0.95, grayscale=False)
@@ -215,21 +217,26 @@ def main():
             Center=pyautogui.center(Part)
             click(Center.x+random.randint(-10,10),Center.y+random.randint(-10,10))
 
-        Part_help=pyautogui.locateAllOnScreen('./others/help.png', confidence=0.9, grayscale=False)
-
+        Part_help=pyautogui.locateAllOnScreen('./others/help.png', confidence=0.95, grayscale=False)
+        #print(Part_help)
         for all_help in Part_help:
-
+            #print(all_help)
             Center=pyautogui.center(all_help)
             click(Center.x+random.randint(-10,10),Center.y+random.randint(-10,10))#识别并打开增援    
             time.sleep(1)
     
             
-            # for i in (13,6,19,9,14,17,16,22,10,12,7,23,3,2,1,'c','e','h','j','G3','G2','G1'):
-            for i in (13,6,10,22,'c','e',17,16,14,3,1,2,'h'):
+            # for i in (13,6,19,9,14,17,16,22,10,12,7,23,19,21,8,3,2,1,'c','e','h','j','G3','G2','G1'):
+            for i in (13,6,10,22,'a','b','c','e',17,16,14,12,3,19,21,8,7,1,2,'h','G1'):
                 photo='./troops/'+str(i)+'.png'
-                Part = pyautogui.locateOnScreen(photo, confidence=0.9, grayscale=False)         # <class 'pyscreeze.Box'>
+                Part = pyautogui.locateOnScreen(photo, confidence=0.8, grayscale=False)         # <class 'pyscreeze.Box'>
                 if Part==None:
-                    print('未识别到图片'+str(i))
+                    print('未识别到图片'+str(i)+what[str(i)])
+                    continue
+                Center=pyautogui.center(Part)
+                rgb=pyautogui.screenshot().getpixel((int(Center.x+random.randint(-10,10)),int(Center.y+random.randint(-10,10))))
+                if rgb[0]==rgb[1] and rgb[1]==rgb[2]:
+                    print('没有库存，不捐赠')
                     continue
                 if type(i)==int:
                     print('捐兵'+str(i))
